8cd5d7acc9bdc88a11524dffce6b8e46b68f2da6
sameesayeed007 <59398279+sameesayeed007@users.noreply.github.com>
2020-06-23 05:03:50 -0700

    Update database.php

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
 $servername="localhost";
 $username="root";
-$password="";
+$password="kookabura";
 $dbname = "sheba";
 $conn=mysqli_connect($servername,$username,$password,$dbname);
 if(!$conn){
